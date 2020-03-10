f7fb9cf5930fc100211622609b7afd13dc26a088
ruslanmuradasilov <muradasilov-r99@mail.ru>
2020-03-10 22:29:58 +0500

    method_of_euler_third_order added

--- ChM/diff_equations.py
+++ ChM/diff_equations.py
@@ -25,12 +25,32 @@ def method_of_euler_second_order(multifunction, n, a, b, y0, C2):
     h = (b - a) / n
     x, y, k1, k2 = [], [], [], []
     C1 = 1 - C2
-    alpha = 1/(2*C2)
+    alpha = 1 / (2 * C2)
     x.append(a)
     y.append(y0)
     for i in range(n):
         k1.append(multifunction(x[i], y[i]) * h)
-        k2.append(multifunction(x[i] + alpha*h, y[i] + alpha*k1[i]) * h)
+        k2.append(multifunction(x[i] + alpha * h, y[i] + alpha * k1[i]) * h)
         x.append(x[i] + h)
-        y.append(y[i] + C1*k1[i] + C2*k2[i])
+        y.append(y[i] + C1 * k1[i] + C2 * k2[i])
+    return x, y
+
+
+def method_of_euler_third_order(multifunction, n, a, b, y0, alpha3, alpha2):
+    h = (b - a) / n
+    x, y, k1, k2, k3 = [], [], [], [], []
+    C2 = (alpha3 / 2 - 1 / 3) / (alpha2 * (alpha3 - alpha2))
+    C3 = (1 / 2 - C2 * alpha2) / alpha3
+    C1 = 1 - C2 - C3
+    beta21 = alpha2
+    beta32 = 1 / (6 * alpha2 * C3)
+    beta31 = alpha3 - beta32
+    x.append(a)
+    y.append(y0)
+    for i in range(n):
+        k1.append(multifunction(x[i], y[i]) * h)
+        k2.append(multifunction(x[i] + alpha2 * h, y[i] + beta21 * k1[i]) * h)
+        k3.append(multifunction(x[i] + alpha3 * h, y[i] + beta31 * k1[i] + beta32*k2[i]) * h)
+        x.append(x[i] + h)
+        y.append(y[i] + C1 * k1[i] + C2 * k2[i] + C3 * k3[i])
     return x, y
--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def function(x):
 
 def multifunction(x, y):
     # return x * x - 2 * y
-    # return y - 2 * x / y
+    return y - 2 * x / y
     # return x + y
-    return (y - 4 * x ** 3) / x
+    # return (y - 4 * x ** 3) / x
 
 
 def accuracy(answer, real):
@@ -25,14 +25,32 @@ def accuracy(answer, real):
 # print(multidimensional_integration.method_simple(multifunction, 200, 100, -1, 1, 0, 1))
 
 # Diff_equations
-x1, y1 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 5)
-x2, y2 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 1)
-x3, y3 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 3 / 4)
-x4, y4 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 1.25)
+# x1, y1 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 5)
+# x2, y2 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 1)
+# x3, y3 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 3 / 4)
+# x4, y4 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 1.25)
+# real = []
+# for i in range(101):
+#     # real.append(sqrt(1 / (4 - 17 / (4 * x1[i] * x1[i]))))
+#     real.append(sqrt(2 * x1[i] + 1))
+#     # real.append(3 * exp(-2 * x[i]) / 4 + x[i] * x[i] / 2 - x[i] / 2 + 1 / 4)
+#     # real.append(exp(x[i]) - x[i] - 1)
+# print(y1[len(y1) - 1])
+# print(y2[len(y2) - 1])
+# print(y3[len(y3) - 1])
+# print(y4[len(y4) - 1])
+# print(f'real: {real[len(real) - 1]}')
+# print(accuracy(y1, real), accuracy(y2, real), accuracy(y3, real), accuracy(y4, real))
+
+x1, y1 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 1, 1 / 2)
+x2, y2 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 1 / 2, 1)
+x3, y3 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 1, 2)
+x4, y4 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 2 / 3, 1 / 3)
+
 real = []
 for i in range(101):
-    real.append(sqrt(1 / (4 - 17 / (4 * x1[i] * x1[i]))))
-    # real.append(sqrt(2 * x1[i] + 1))
+    # real.append(sqrt(1 / (4 - 17 / (4 * x1[i] * x1[i]))))
+    real.append(sqrt(2 * x1[i] + 1))
     # real.append(3 * exp(-2 * x[i]) / 4 + x[i] * x[i] / 2 - x[i] / 2 + 1 / 4)
     # real.append(exp(x[i]) - x[i] - 1)
 print(y1[len(y1) - 1])
@@ -41,6 +59,7 @@ print(y3[len(y3) - 1])
 print(y4[len(y4) - 1])
 print(f'real: {real[len(real) - 1]}')
 print(accuracy(y1, real), accuracy(y2, real), accuracy(y3, real), accuracy(y4, real))
+
 plt.plot(x1, y1, x2, y2, x3, y3, x4, y4)
 plt.plot(x1, real)
 plt.show()
