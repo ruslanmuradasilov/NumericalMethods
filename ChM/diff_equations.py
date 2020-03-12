176d7522de65dead3c039590686a04ddc0d63c5e
ruslanmuradasilov <muradasilov-r99@mail.ru>
2020-03-12 20:36:03 +0500

    method_of_euler_third_order added method_of_euler_fourth_order added

--- ChM/diff_equations.py
+++ ChM/diff_equations.py
@@ -54,3 +54,35 @@ def method_of_euler_third_order(multifunction, n, a, b, y0, alpha3, alpha2):
         x.append(x[i] + h)
         y.append(y[i] + C1 * k1[i] + C2 * k2[i] + C3 * k3[i])
     return x, y
+
+def method_of_euler_fourth_order(multifunction, n, a, b, y0, alpha2, alpha3, alpha4 = 1):
+    h = (b - a) / n
+    delta = alpha2 * alpha3 * alpha4 * (alpha3 - alpha2) * (alpha4 - alpha3) * (alpha4 - alpha2)
+
+    if delta == 0:
+        return None
+
+    alpha4 = 1
+    c2 = (2 * alpha3 - 1) / (12 * alpha2 * (alpha3 - alpha2) * (1 - alpha3))
+    c3 = (1 - 2 * alpha2) / (12 * alpha3 * (1 - alpha2) * (1 - alpha3))
+    c4 = (6 * alpha2 * alpha3 - 4 * alpha2 - 4 * alpha3 + 3) / (12 * (1 - alpha2) * (1 - alpha3))
+    beta42 = -(4 * alpha3 ** 2 - alpha2 - 5 * alpha3 + 2) / (24 * c4 * alpha3 * (alpha3 - alpha2))
+    beta43 = (1 - 2 * alpha2) / (24 * c4 * alpha3 * (alpha3 - alpha2))
+    beta41 = 1 - beta42 - beta43
+    beta21 = alpha2
+    beta32 = 1 / (24 * c4 * beta43 * alpha2)
+    beta31 = alpha3 - beta32
+    c1 = 1 - c2 - c3 - c4
+
+    x, y, k1, k2, k3, k4 = [], [], [], [], [], []
+
+    x.append(a)
+    y.append(y0)
+    for i in range(n):
+        k1.append(multifunction(x[i], y[i]) * h)
+        k2.append(multifunction(x[i] + alpha2 * h, y[i] + beta21 * k1[i]) * h)
+        k3.append(multifunction(x[i] + alpha3 * h, y[i] + beta31 * k1[i] + beta32 * k2[i]) * h)
+        k4.append(multifunction(x[i] + alpha4 * h, y[i] + beta41 * k1[i] + beta42 * k2[i] + beta43 * k3[i]) * h)
+        x.append(x[i] + h)
+        y.append(y[i] + c1 * k1[i] + c2 * k2[i] + c3 * k3[i] + c4 * k4[i])
+    return x, y
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def function(x):
 
 
 def multifunction(x, y):
-    # return x * x - 2 * y
+    # return (x * x - 2 * y) / (2 * x)
     return y - 2 * x / y
     # return x + y
     # return (y - 4 * x ** 3) / x
+    # return y * tan(x) + sin(x)
 
 
 def accuracy(answer, real):
@@ -22,44 +23,43 @@ def accuracy(answer, real):
 
 
 # Integration
+
 # print(multidimensional_integration.method_simple(multifunction, 200, 100, -1, 1, 0, 1))
 
 # Diff_equations
-# x1, y1 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 5)
-# x2, y2 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 1)
-# x3, y3 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 3 / 4)
-# x4, y4 = diff_equations.method_of_euler_second_order(multifunction, 100, 1, 2, 2, 1.25)
+
+# x1, y1 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 1.5, 0, 1, 1 / 2)
+# x2, y2 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 1.5, 0, 1 / 2, 1)
+# x3, y3 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 1.5, 0, 1, 2)
+# x4, y4 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 1.5, 0, 2 / 3, 1 / 3)
+#
 # real = []
 # for i in range(101):
 #     # real.append(sqrt(1 / (4 - 17 / (4 * x1[i] * x1[i]))))
-#     real.append(sqrt(2 * x1[i] + 1))
+#     # real.append(sqrt(2 * x1[i] + 1))
 #     # real.append(3 * exp(-2 * x[i]) / 4 + x[i] * x[i] / 2 - x[i] / 2 + 1 / 4)
 #     # real.append(exp(x[i]) - x[i] - 1)
+#     # real.append(1 / (3 * x1[i] / 2 - x1[i] * x1[i] / 2))
+#     real.append((1 - cos(2 * x1[i])) / cos(x1[i]))
 # print(y1[len(y1) - 1])
 # print(y2[len(y2) - 1])
 # print(y3[len(y3) - 1])
 # print(y4[len(y4) - 1])
 # print(f'real: {real[len(real) - 1]}')
 # print(accuracy(y1, real), accuracy(y2, real), accuracy(y3, real), accuracy(y4, real))
+#
+# plt.plot(x1, y1, x2, y2, x3, y3, x4, y4)
+# plt.plot(x1, real)
+# plt.show()
 
-x1, y1 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 1, 1 / 2)
-x2, y2 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 1 / 2, 1)
-x3, y3 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 1, 2)
-x4, y4 = diff_equations.method_of_euler_third_order(multifunction, 100, 0, 5, 1, 2 / 3, 1 / 3)
-
+x, y = diff_equations.method_of_euler_fourth_order(multifunction, 100, 0, 5, 1, 1/3 - 0.01, 2/3 - 0.001)
 real = []
 for i in range(101):
-    # real.append(sqrt(1 / (4 - 17 / (4 * x1[i] * x1[i]))))
-    real.append(sqrt(2 * x1[i] + 1))
-    # real.append(3 * exp(-2 * x[i]) / 4 + x[i] * x[i] / 2 - x[i] / 2 + 1 / 4)
-    # real.append(exp(x[i]) - x[i] - 1)
-print(y1[len(y1) - 1])
-print(y2[len(y2) - 1])
-print(y3[len(y3) - 1])
-print(y4[len(y4) - 1])
+    real.append(sqrt(2 * x[i] + 1))
+print(y[len(y) - 1])
 print(f'real: {real[len(real) - 1]}')
-print(accuracy(y1, real), accuracy(y2, real), accuracy(y3, real), accuracy(y4, real))
+print(accuracy(y, real))
 
-plt.plot(x1, y1, x2, y2, x3, y3, x4, y4)
-plt.plot(x1, real)
+plt.plot(x, y)
+plt.plot(x, real)
 plt.show()
