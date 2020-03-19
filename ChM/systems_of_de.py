694875d9cc05ffdf37d8d32f657c7609d7525f6d
ruslanmuradasilov <muradasilov-r99@mail.ru>
2020-03-19 13:44:13 +0500

    systems_of_de.py added

---
+++ ChM/systems_of_de.py
@@ -0,0 +1,123 @@
+def method_of_euler(multifunction1, multifunction2, n, a, b, y10, y20, epsilon=None):
+    if epsilon == None:
+        h = (b - a) / n
+        t, y1, y2 = [], [], []
+        t.append(a)
+        y1.append(y10)
+        y2.append(y20)
+        for i in range(n):
+            t.append(t[i] + h)
+            y1.append(y1[i] + multifunction1(y1[i], y2[i]) * h)
+            y2.append(y2[i] + multifunction2(y1[i], y2[i]) * h)
+        return t, y1, y2
+    else:
+        i = 1
+        while True:
+            sum1, sum2 = 0, 0
+            x1, result11, result12 = method_of_euler(multifunction1, multifunction2, i * n, a, b, y10, y20)
+            x2, result21, result22 = method_of_euler(multifunction1, multifunction2, i * 2 * n, a, b, y10, y20)
+            for j in range(len(x1) - 1):
+                sum1 += (result11[j] - result21[2 * j]) ** 2
+                sum2 += (result12[j] - result22[2 * j]) ** 2
+            if sum1 < epsilon and sum2 < epsilon:
+                return x2, result21, result22, i * 2 * n
+            i *= 2
+
+
+def method_of_euler_second_order(multifunction1, multifunction2, n, a, b, y10, y20, epsilon=None):
+    if epsilon == None:
+        h = (b - a) / n
+        t, y1, y2, k11, k21, k12, k22 = [], [], [], [], [], [], []
+        t.append(a)
+        y1.append(y10)
+        y2.append(y20)
+        for i in range(n):
+            t.append(t[i] + h)
+            k11.append(multifunction1(y1[i], y2[i]) * h)
+            k21.append(multifunction2(y1[i], y2[i]) * h)
+            k12.append(multifunction1(y1[i] + k11[i] / 2, y2[i] + k21[i] / 2) * h)
+            k22.append(multifunction2(y1[i] + k11[i] / 2, y2[i] + k21[i] / 2) * h)
+            y1.append(y1[i] + 1 / 2 * k11[i] + 1 / 2 * k12[i])
+            y2.append(y2[i] + 1 / 2 * k21[i] + 1 / 2 * k22[i])
+        return t, y1, y2
+    else:
+        i = 1
+        while True:
+            sum1, sum2 = 0, 0
+            x1, result11, result12 = method_of_euler_second_order(multifunction1, multifunction2, i * n, a, b, y10, y20)
+            x2, result21, result22 = method_of_euler_second_order(multifunction1, multifunction2, i * 2 * n, a, b, y10,
+                                                                  y20)
+            for j in range(len(x1) - 1):
+                sum1 += (result11[j] - result21[2 * j]) ** 2
+                sum2 += (result12[j] - result22[2 * j]) ** 2
+            if sum1 < epsilon and sum2 < epsilon:
+                return x2, result21, result22, i * 2 * n
+            i *= 2
+
+
+def method_of_euler_third_order(multifunction1, multifunction2, n, a, b, y10, y20, epsilon=None):
+    if epsilon == None:
+        h = (b - a) / n
+        t, y1, y2, k11, k21, k12, k22, k13, k23 = [], [], [], [], [], [], [], [], []
+        t.append(a)
+        y1.append(y10)
+        y2.append(y20)
+        for i in range(n):
+            t.append(t[i] + h)
+            k11.append(multifunction1(y1[i], y2[i]) * h)
+            k21.append(multifunction2(y1[i], y2[i]) * h)
+            k12.append(multifunction1(y1[i] + k11[i] / 2, y2[i] + k21[i] / 2) * h)
+            k22.append(multifunction2(y1[i] + k11[i] / 2, y2[i] + k21[i] / 2) * h)
+            k13.append(multifunction1(y1[i] + k12[i] / 2, y2[i] + k22[i] / 2) * h)
+            k23.append(multifunction2(y1[i] + k12[i] / 2, y2[i] + k22[i] / 2) * h)
+            y1.append(y1[i] + 1 / 6 * k11[i] + 1 / 2 * k12[i] + 1/3 * k13[i])
+            y2.append(y2[i] + 1 / 6 * k21[i] + 1 / 2 * k22[i] + 1/3 * k23[i])
+        return t, y1, y2
+    else:
+        i = 1
+        while True:
+            sum1, sum2 = 0, 0
+            x1, result11, result12 = method_of_euler_third_order(multifunction1, multifunction2, i * n, a, b, y10, y20)
+            x2, result21, result22 = method_of_euler_third_order(multifunction1, multifunction2, i * 2 * n, a, b, y10,
+                                                                 y20)
+            for j in range(len(x1) - 1):
+                sum1 += (result11[j] - result21[2 * j]) ** 2
+                sum2 += (result12[j] - result22[2 * j]) ** 2
+            if sum1 < epsilon and sum2 < epsilon:
+                return x2, result21, result22, i * 2 * n
+            i *= 2
+
+
+def method_of_euler_fourth_order(multifunction1, multifunction2, n, a, b, y10, y20, epsilon=None):
+    if epsilon == None:
+        h = (b - a) / n
+        t, y1, y2, k11, k21, k12, k22, k13, k23, k14, k24 = [], [], [], [], [], [], [], [], [], [], []
+        t.append(a)
+        y1.append(y10)
+        y2.append(y20)
+        for i in range(n):
+            t.append(t[i] + h)
+            k11.append(multifunction1(y1[i], y2[i]) * h)
+            k21.append(multifunction2(y1[i], y2[i]) * h)
+            k12.append(multifunction1(y1[i] + k11[i] / 2, y2[i] + k21[i] / 2) * h)
+            k22.append(multifunction2(y1[i] + k11[i] / 2, y2[i] + k21[i] / 2) * h)
+            k13.append(multifunction1(y1[i] + k12[i] / 2, y2[i] + k22[i] / 2) * h)
+            k23.append(multifunction2(y1[i] + k12[i] / 2, y2[i] + k22[i] / 2) * h)
+            k14.append(multifunction1(y1[i] + k13[i] / 2, y2[i] + k23[i] / 2) * h)
+            k24.append(multifunction2(y1[i] + k13[i] / 2, y2[i] + k23[i] / 2) * h)
+            y1.append(y1[i] + 1 / 6 * k11[i] + 1 / 3 * k12[i] + 1 / 3 * k13[i] + 1 / 6 * k14[i])
+            y2.append(y2[i] + 1 / 6 * k21[i] + 1 / 3 * k22[i] + 1 / 3 * k23[i] + 1 / 6 * k24[i])
+        return t, y1, y2
+    else:
+        i = 1
+        while True:
+            sum1, sum2 = 0, 0
+            x1, result11, result12 = method_of_euler_fourth_order(multifunction1, multifunction2, i * n, a, b, y10, y20)
+            x2, result21, result22 = method_of_euler_fourth_order(multifunction1, multifunction2, i * 2 * n, a, b, y10,
+                                                                  y20)
+            for j in range(len(x1) - 1):
+                sum1 += (result11[j] - result21[2 * j]) ** 2
+                sum2 += (result12[j] - result22[2 * j]) ** 2
+            if sum1 < epsilon and sum2 < epsilon:
+                return x2, result21, result22, i * 2 * n
+            i *= 2
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ChM import integration, multidimensional_integration, diff_equations
+from ChM import integration, multidimensional_integration, diff_equations, systems_of_de
 import matplotlib.pyplot as plt
 from math import sin, cos, sqrt, exp, log, e, fabs, tan
 
@@ -14,6 +14,14 @@ def multifunction(x, y):
     # return (y - 4 * x ** 3) / x
     # return y * tan(x) + sin(x)
     return -50 * (y + cos(x))
+
+
+def multifunction1(x, y):
+    return x - 2 * y
+
+
+def multifunction2(x, y):
+    return x - y
 
 
 def accuracy(answer, real):
@@ -72,17 +80,26 @@ def accuracy(answer, real):
 # plt.show()
 
 n0 = 20
-x1, y1, n = diff_equations.method_of_adams_fourth_order(multifunction, n0, 0, 10, 0, epsilon=0.000001)
-print(f'n = {n}')
-real = []
-for i in range(n + 1):
-    real.append((-50 * (50 * cos(x1[i]) + sin(x1[i])) + 2500 * exp(-50 * x1[i])) / 2501)
-    # real.append(sqrt(2 * x1[i] + 1))
-    # real.append((1 - cos(2 * x1[i])) / (4 * cos(x1[i])))
-print(y1[len(y1) - 1])
-print(f'real: {real[len(real) - 1]}')
-print(accuracy(y1, real))
-
-plt.plot(x1, y1)
-plt.plot(x1, real)
-plt.show()
+t1, y11, y21, n1 = systems_of_de.method_of_euler(multifunction1, multifunction2, n0, 0, 5, 1, 2, epsilon=0.001)
+print(f'n1 = {n1}')
+t2, y12, y22, n2 = systems_of_de.method_of_euler_second_order(multifunction1, multifunction2, n0, 0, 5, 1, 2, epsilon=0.001)
+print(f'n2 = {n2}')
+t3, y13, y23, n3 = systems_of_de.method_of_euler_third_order(multifunction1, multifunction2, n0, 0, 5, 1, 2, epsilon=0.001)
+print(f'n3 = {n3}')
+t4, y14, y24, n4 = systems_of_de.method_of_euler_fourth_order(multifunction1, multifunction2, n0, 0, 5, 1, 2, epsilon=0.001)
+print(f'n4 = {n4}')
+# real1, real2 = [], []
+# for i in range(n4 + 1):
+#     real1.append(cos(t4[i]) - 3 * sin(t4[i]))
+#     real2.append(2 * cos(t4[i]) - sin(t4[i]))
+# print(y14[len(y14) - 1])
+# print(y24[len(y24) - 1])
+# print(f'real: {real1[len(real1) - 1]}')
+# print(f'real: {real2[len(real2) - 1]}')
+# print(accuracy(y14, real1), accuracy(y24, real2))
+#
+# plt.plot(t4, y14)
+# # plt.plot(x1, y2)
+# plt.plot(t4, real1)
+# # plt.plot(x1, real2)
+# plt.show()
